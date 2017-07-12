7bc06ed4073d5fd605d5cbf8341abb8a6f97a49e
Daichi Nomura <BI912463@jp.ibm.com>
2017-07-12 18:34:24 +0900

    repair

--- Game1/src/janken3/Judge3.java
+++ Game1/src/janken3/Judge3.java
@@ -2,27 +2,44 @@ package janken3;
 
 public class Judge3 {
 	public void startJanken(Player3 p1, Player3 p2) {
-		System.out.println("ジャンケン開始");
-		
-		boolean loop = false;
-		while(loop == false) {
+		System.out.println("【ジャンケン開始】\n");
+
+		try {
+			Thread.sleep(1000);
+		} catch(InterruptedException e) {
+		}
+
+		boolean loop = true;
+		int count = 1;
+		while(loop == true) {
 			boolean janken = false;
 			do {
+				System.out.println((count) + "回目！");
+				count++;
 				Player3 winner = judgeJanken(p1, p2);
+
+				try {
+					Thread.sleep(1000);
+				} catch(InterruptedException e) {
+				}
+
 				if(winner != null) {
 					System.out.println(winner.getName() + "の勝ち\n");
+					System.out.println("------------------");
 					winner.notifyResult(true);
-					janken = true;
+					janken = false;
 				} else {
 					System.out.println("あいこ！再勝負！\n");
+					System.out.println("------------------");
+					janken = true;
 				}
-			} while(janken == false);
-			
+			} while(janken == true);
+
 			if(p1.getWinCount() == 3 || p2.getWinCount() == 3) {
-				loop = true;
+				loop = false;
 			}
 		}
-		
+
 		Player3 finalWinner = finalJudgeJanken(p1, p2);
 		System.out.println(p1.getWinCount() + ":" + p2.getWinCount());
 		if(finalWinner != null) {
@@ -31,18 +48,18 @@ public class Judge3 {
 			System.out.println("引き分け");
 		}
 	}
-	
+
 	private Player3 judgeJanken(Player3 p1, Player3 p2) {
 		Player3 winner = null;
 		int p1Hand = p1.showHand();
 		int p2Hand = p2.showHand();
-		
-		System.out.print(p1.getName());
+
+		System.out.print(p1.getName() + " ");
 		printHand(p1Hand);
 		System.out.println(" vs");
-		System.out.print(p2.getName());
+		System.out.print(p2.getName() + " ");
 		printHand(p2Hand);
-		
+
 		if((p1Hand == Player3.STN && p2Hand == Player3.SCI)
 				|| (p1Hand == Player3.SCI && p2Hand == Player3.PAP)
 				|| (p1Hand == Player3.PAP && p2Hand == Player3.STN)) {
@@ -54,12 +71,12 @@ public class Judge3 {
 		}
 		return winner;
 	}
-	
+
 	private Player3 finalJudgeJanken(Player3 p1, Player3 p2) {
 		Player3 finalWinner = null;
 		int p1Win = p1.getWinCount();
 		int p2Win = p2.getWinCount();
-		
+
 		if(p1Win > p2Win) {
 			finalWinner = p1;
 		} else if(p1Win < p2Win) {
@@ -67,21 +84,21 @@ public class Judge3 {
 		}
 		return finalWinner;
 	}
-	
+
 	private void printHand(int hand) {
 		switch(hand) {
 		case Player3.STN :
 			System.out.println("グー");
 			break;
-			
+
 		case Player3.SCI :
 			System.out.println("チョキ");
 			break;
-			
+
 		case Player3.PAP :
 			System.out.println("パー");
 			break;
-			
+
 		default :
 			break;
 		}
